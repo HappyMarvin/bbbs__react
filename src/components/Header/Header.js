a06a70c49d4eec5d5f7329660a99a181181e8b4b
Happy Marvin <HappyMarvin@yandex.ru>
2021-05-21 15:23:14 +0300

    fix prettier

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -43,32 +43,111 @@ function Header() {
 
         <nav class="header__action">
           <a class="header__button-search" href="./pages-html/search.html"></a>
-          <a href="#" class="header__button-login header__button-login_unauthorized"></a>
+          <a
+            href="#"
+            class="header__button-login header__button-login_unauthorized"
+          ></a>
         </nav>
       </div>
       <div class="header__burger header__burger_hidden">
         <div class="header__burger-wrapper">
           <nav class="header__menu-burger">
             <ul class="header__burger-list">
-              <li class="header__burger-item"><a href="./pages-html/about.html" class="header__burger-link">О проекте</a></li>
-              <li class="header__burger-item"><a href="#" class="header__burger-link calender-open">Календарь</a></li>
-              <li class="header__burger-item"><a href="./pages-html/where_to_go.html" class="header__burger-link">Куда пойти</a></li>
-              <li class="header__burger-item"><a href="./pages-html/questions.html" class="header__burger-link">Вопросы</a></li>
-              <li class="header__burger-item"><a href="./pages-html/read_watch.html" class="header__burger-link">Читать и смотреть</a></li>
-              <li class="header__burger-item"><a href="./pages-html/questions.html" class="header__burger-link">Права детей</a></li>
-              <li class="header__burger-item"><a href="#" class="header__burger-link">Истории</a></li>
+              <li class="header__burger-item">
+                <a href="./pages-html/about.html" class="header__burger-link">
+                  О проекте
+                </a>
+              </li>
+              <li class="header__burger-item">
+                <a href="#" class="header__burger-link calender-open">
+                  Календарь
+                </a>
+              </li>
+              <li class="header__burger-item">
+                <a
+                  href="./pages-html/where_to_go.html"
+                  class="header__burger-link"
+                >
+                  Куда пойти
+                </a>
+              </li>
+              <li class="header__burger-item">
+                <a
+                  href="./pages-html/questions.html"
+                  class="header__burger-link"
+                >
+                  Вопросы
+                </a>
+              </li>
+              <li class="header__burger-item">
+                <a
+                  href="./pages-html/read_watch.html"
+                  class="header__burger-link"
+                >
+                  Читать и смотреть
+                </a>
+              </li>
+              <li class="header__burger-item">
+                <a
+                  href="./pages-html/questions.html"
+                  class="header__burger-link"
+                >
+                  Права детей
+                </a>
+              </li>
+              <li class="header__burger-item">
+                <a href="#" class="header__burger-link">
+                  Истории
+                </a>
+              </li>
             </ul>
           </nav>
           <nav class="header__menu-burger">
             <ul class="header__burger-list">
-              <li class="header__burger-item"><a href="#" class="header__burger-link" target="_blank" rel="noopener">facebook</a></li>
-              <li class="header__burger-item"><a href="#" class="header__burger-link" target="_blank" rel="noopener">vkontakte</a></li>
-              <li class="header__burger-item"><a href="#" class="header__burger-link" target="_blank" rel="noopener">instagram</a></li>
-              <li class="header__burger-item"><a href="#" class="header__burger-link" target="_blank" rel="noopener">youtube</a></li>
+              <li class="header__burger-item">
+                <a
+                  href="#"
+                  class="header__burger-link"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  facebook
+                </a>
+              </li>
+              <li class="header__burger-item">
+                <a
+                  href="#"
+                  class="header__burger-link"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  vkontakte
+                </a>
+              </li>
+              <li class="header__burger-item">
+                <a
+                  href="#"
+                  class="header__burger-link"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  instagram
+                </a>
+              </li>
+              <li class="header__burger-item">
+                <a
+                  href="#"
+                  class="header__burger-link"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  youtube
+                </a>
+              </li>
             </ul>
           </nav>
         </div>
       </div>
     </header>
-  )
+  );
 }
