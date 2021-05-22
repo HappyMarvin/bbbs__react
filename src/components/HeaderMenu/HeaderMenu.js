e965ae697d234a8f70b82af1315dc8086ab28b7e
Voltrein-code <mr-voltrein@yandex.ru>
2021-05-22 10:16:58 +0300

    refactor: change names of classes

--- src/components/HeaderMenu/HeaderMenu.js
+++ src/components/HeaderMenu/HeaderMenu.js
@@ -4,35 +4,35 @@ import "./HeaderMenu.css";
 
 export default function HeaderMenu() {
   return (
-    <nav className="header__menu">
-      <ul className="header__list">
-        <li className="header__list-item calender-open">
-          <Link to="/" className="header__list-link">
+    <nav className="header-menu">
+      <ul className="header-menu__list">
+        <li className="header-menu__list-item calender-open">
+          <Link to="/" className="header-menu__list-link">
             Календарь
           </Link>
         </li>
-        <li className="header__list-item">
-          <Link to="/" className="header__list-link">
+        <li className="header-menu__list-item">
+          <Link to="/" className="header-menu__list-link">
             Куда пойти
           </Link>
         </li>
-        <li className="header__list-item">
-          <Link to="/" className="header__list-link">
+        <li className="header-menu__list-item">
+          <Link to="/" className="header-menu__list-link">
             Вопросы
           </Link>
         </li>
-        <li className="header__list-item">
-          <Link to="/" className="header__list-link">
+        <li className="header-menu__list-item">
+          <Link to="/" className="header-menu__list-link">
             Читать и смотреть
           </Link>
         </li>
-        <li className="header__list-item">
-          <Link to="/" className="header__list-link">
+        <li className="header-menu__list-item">
+          <Link to="/" className="header-menu__list-link">
             Права детей
           </Link>
         </li>
-        <li className="header__list-item">
-          <Link to="/" className="header__list-link">
+        <li className="header-menu__list-item">
+          <Link to="/" className="header-menu__list-link">
             Истории
           </Link>
         </li>
