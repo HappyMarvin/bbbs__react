f979fa04b616554c91473523e3c47efa6fa6fda5
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-05-23 17:54:08 +0300

    feat: add organization on about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,19 +2,51 @@ import React from "react";
 import Logo from "../Logo/Logo";
 import "./About.css";
 
+import logoBlue from "../../images/logoSBSS-blue.svg";
+
 const About = ({ mix }) => {
   return (
     <main className={`about ${mix}`}>
       <section className="about__intro" aria-label="Кратко о проекте">
-        <div className="about__intro-text">
+        <p className="about__intro-text">
           Наставники.про&nbsp;&mdash; цифоровая информационная платформа
           огрганизации &laquo;Старшие Братья Старшие Сестры&raquo;. Созданная
           для поддержки наставников программы.
-        </div>
+        </p>
       </section>
 
       <section className="about__logo" aria-label="Логотип">
-        <Logo place="about" />
+        <Logo place="about" image={logoBlue} />
+      </section>
+
+      <section className="about__organization">
+        <div className="about__organization-circle">
+          <h2 className="about__organization-title">Об организации</h2>
+        </div>
+        <article className="about__organization-text">
+          <p className="about__organization-paragraph">
+            &laquo;Старшие Братья Старшие Сестры&raquo;&nbsp;&mdash;
+            межрегиональная общественная организация содействия воспитанию
+            подрастающего поколения. Мы&nbsp;поддерживаем детей, которым
+            требуется внимание: оставшихся без попечения родителей, приемных,
+            детей из&nbsp;неполных, многодетных или неблагополучных семей, детей
+            с&nbsp;ограниченными возможностями.
+          </p>
+          <p className="about__organization-paragraph">
+            Любому человеку, тем более ребенку, необходимо общение. Без него
+            дети растут неуверенными и&nbsp;замкнутыми. Одиночество токсично,
+            а&nbsp;самое надежное противоядие &mdash; дружба.
+          </p>
+          <p className="about__organization-paragraph">
+            Мы&nbsp;помогаем детям, которым не&nbsp;хватает поддержки взрослого
+            друга, &laquo;Младшим&raquo;. Таким другом становится наш волонтер,
+            &laquo;Старший&raquo;. Он&nbsp;принимает ребенка, какой
+            он&nbsp;есть, поддерживает, помогает раскрыть потенциал,
+            почувствовать уверенность в&nbsp;своих силах, узнать элементарные
+            вещи о&nbsp;жизни, адаптироваться и&nbsp;полноценно участвовать
+            в&nbsp;жизни общества.
+          </p>
+        </article>
       </section>
     </main>
   );
