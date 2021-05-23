f4b09886ea781a8d4b51d12b511a2d89cfe13a2d
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-05-23 14:48:48 +0300

    feat: add fonts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./vendor/normalize.css";
+import "./vendor/fonts/fonts.css";
 import "./index.css";
 import App from "./components/App/App";
 
