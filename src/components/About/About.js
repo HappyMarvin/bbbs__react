09188a1d5828dc2463a8c32b83ad476d3425e778
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-05-23 14:39:43 +0300

    feat: add intro to about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./About.css";
 
 const About = ({ mix }) => {
-  return <main className={`about ${mix}`}>about</main>;
+  return (
+    <main className={`about ${mix}`}>
+      <section className="about__intro" aria-label="Кратко о проекте">
+        <div className="about__intro-text">
+          Наставники.про&nbsp;&mdash; цифоровая информационная платформа
+          огрганизации &laquo;Старшие Братья Старшие Сестры&raquo;. Созданная
+          для поддержки наставников программы.
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default About;
