8dce74cc75460829ebace9c34b2025b9b583a13e
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-06-04 23:54:01 +0300

    feat: add header sublinks

--- src/components/HeaderMenu/HeaderMenu.js
+++ src/components/HeaderMenu/HeaderMenu.js
@@ -13,10 +13,24 @@ export default function HeaderMenu() {
     <nav className="header-menu">
       <ul className="header-menu__list">
         {headerLinks.map((item, index) => (
-          <li key={index} className="header-menu__list-item calender-open">
+          <li key={index} className="header-menu__list-item">
             <Link to={item.link} className="header-menu__list-link">
               {item.title}
             </Link>
+            {item.children && (
+              <ul className="header-menu__subgroup">
+                {Object.values(item.children).map((element, i) => (
+                  <li key={i} className="header-menu__subitem">
+                    <Link
+                      to={`${item.link}${element.link}`}
+                      className="header-menu__sublink"
+                    >
+                      {element.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
         ))}
       </ul>
