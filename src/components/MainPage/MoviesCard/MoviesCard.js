72129e14a967587a3a79f3cae5da70bfd1fc1aa7
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-06-05 19:37:37 +0300

    feat: fix class and className

--- src/components/MainPage/MoviesCard/MoviesCard.js
+++ src/components/MainPage/MoviesCard/MoviesCard.js
@@ -4,17 +4,21 @@ import "./MoviesCard.css";
 
 export default function MoviesCard({ movie }) {
   return (
-    <li class="video-card">
-      <img class="video-card__image" src={movie.imageLink} alt={movie.title} />
-      <div class="video-card__description">
-        <div class="video-card__containter">
-          <span class="video-card__category">Рубрика</span>
-          <span class="video-card__category">Рубрика</span>
+    <li className="video-card">
+      <img
+        className="video-card__image"
+        src={movie.imageLink}
+        alt={movie.title}
+      />
+      <div className="video-card__description">
+        <div className="video-card__containter">
+          <span className="video-card__category">Рубрика</span>
+          <span className="video-card__category">Рубрика</span>
         </div>
-        <h2 class="video__title video-card__title">{movie.title}</h2>
-        <p class="video-card__caption caption">{movie.description}</p>
+        <h2 className="video__title video-card__title">{movie.title}</h2>
+        <p className="video-card__caption caption">{movie.description}</p>
         <a
-          class="clickable-element"
+          className="clickable-element"
           href={movie.trailerLink}
           target="_blank"
           rel="noopener noreferrer"
