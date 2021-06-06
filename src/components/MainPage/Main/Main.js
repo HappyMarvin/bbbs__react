8cf96b9cf4d388c5139ad916db2e15055dfbc51f
Ianina Begun <ianina.begun@yandex.ru>
2021-06-06 20:55:59 +0300

    fixes in mainPage

--- src/components/MainPage/Main/Main.js
+++ src/components/MainPage/Main/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Bbbs from "../../Bbbs/Bbbs";
+import EventCard from "../EventCard/EventCard";
 import Events from "../Events/Events";
 import Story from "../Story/Story";
 import ArticlePreview from "../ArticlePreview/ArticlePreview";
@@ -8,20 +9,14 @@ import MoviesPreviewList from "../MoviesPreviewList/MoviesPreviewList";
 import GraduateVideo from "../GraduateVideo/GraduateVideo";
 import WidgetQuestions from "../WidgetQuestions/WidgetQuestions";
 
-/* import {moviePic} from "../../../images/" */
-
 import "./Main.css";
 
-export default function Main(/* { isLoggedin, event } */ { mix }) {
+export default function Main({ isLoggedIn, event, mix, handleOpenPopup }) {
   return (
     <main className={`main`}>
       <section className="mainpage content main__section">
         <section className="mainpage__intro">
-          {/*
-          пока нет merge и protected route напишу так. Логика по заданию - если user
-          авторизован, то ближайшее событие из календаря, если нет - то описание проекта
-          isLoggedIn ? <Calendar event={event}/> : <Bbbs/> */}
-          <Bbbs />
+          {isLoggedIn ? <EventCard /> : <Bbbs />}
           <Story />
         </section>
 
@@ -38,50 +33,12 @@ export default function Main(/* { isLoggedin, event } */ { mix }) {
           <MoviesPreviewList />
         </section>
 
-        <section className="video-bigcard">
+        <section className="video-bigcard" onClick={() => handleOpenPopup()}>
           <GraduateVideo />
         </section>
 
         <section className="mainpage__blocks-col">
           <WidgetQuestions />
-          {/* <iframe
-            className="facebook"
-            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBigBrothers.BigSisters.Russia&tabs=timeline&width=420&height=627&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId"
-            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-          ></iframe>
-          <ul className="questions questions_place_maipage">
-            <li className="question">
-              <a className="mainlink" href="#"></a>
-              <h3 className="question__title">
-                Я&nbsp;боюсь, что ребёнок ко&nbsp;мне слишком сильно привяжется.
-                Что делать?
-              </h3>
-              <div className="question__tags">
-                <p className="tag tag_theme_white">рубрика</p>
-              </div>
-            </li>
-            <li className="question">
-              <a className="mainlink" href="#"></a>
-              <h3 className="question__title">
-                Возможно&nbsp;ли продлить срок участия в&nbsp;программе, если
-                и&nbsp;я&nbsp;и&nbsp;мой &laquo;младший&raquo; хотим остаться
-                в&nbsp;программе?
-              </h3>
-              <div className="question__tags">
-                <p className="tag tag_theme_white">рубрика</p>
-              </div>
-            </li>
-            <li className="question">
-              <a className="mainlink" href="#"></a>
-              <h3 className="question__title">
-                Что делать если Ваш младший решил закрыть пару, т.к. слишком
-                занят с&nbsp;учебой и&nbsp;друзьями?
-              </h3>
-              <div className="question__tags">
-                <p className="tag tag_theme_white">рубрика</p>
-              </div>
-            </li>
-          </ul> */}
         </section>
 
         <section className="mainpage__block">
--- src/components/MainPage/WidgetQuestions/WidgetQuestions.js
+++ src/components/MainPage/WidgetQuestions/WidgetQuestions.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./WidgetQuestions.css";
 
 export default function WidgetQuestions() {
+  const iframeUrl = `https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBigBrothers.BigSisters.Russia&tabs=timeline&width=420px&height=627px&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId`;
   return (
     <>
-      <iframe
-        title="facebook-widget"
-        className="facebook"
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBigBrothers.BigSisters.Russia&tabs=timeline&width=420&height=627&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-      ></iframe>
+      <div className="facebook">
+        <iframe
+          title="facebook-widget"
+          className="facebook__widget"
+          src={iframeUrl}
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        ></iframe>
+      </div>
       <ul className="questions questions_place_maipage">
         <li className="question">
-          <a className="mainlink" href="#"></a>
+          <Link className="mainlink" to="/questions"></Link>
           <h3 className="question__title">
             Я&nbsp;боюсь, что ребёнок ко&nbsp;мне слишком сильно привяжется. Что
             делать?
@@ -23,7 +27,7 @@ export default function WidgetQuestions() {
           </div>
         </li>
         <li className="question">
-          <a className="mainlink" href="#"></a>
+          <Link className="mainlink" to="/questions"></Link>
           <h3 className="question__title">
             Возможно&nbsp;ли продлить срок участия в&nbsp;программе, если
             и&nbsp;я&nbsp;и&nbsp;мой &laquo;младший&raquo; хотим остаться
@@ -34,7 +38,7 @@ export default function WidgetQuestions() {
           </div>
         </li>
         <li className="question">
-          <a className="mainlink" href="#"></a>
+          <Link className="mainlink" to="/questions"></Link>
           <h3 className="question__title">
             Что делать если Ваш младший решил закрыть пару, т.к. слишком занят
             с&nbsp;учебой и&nbsp;друзьями?
